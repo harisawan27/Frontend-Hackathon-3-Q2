16c79077f4aa0ffc3cb58bea03fe718cd32eab6f
Muhammad Haris Awan <160159398+harisawan27@users.noreply.github.com>
2025-01-23 02:59:36 +0500

    Create SocialLinks.tsx

---
+++ app/components/SocialLinks.tsx
@@ -0,0 +1,53 @@
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+const SocialLinks = () => {
+  return (
+    <div className="flex space-x-4">
+      {/* Twitter */}
+      <a
+        href="https://x.com/harisawan27"
+        aria-label="Twitter"
+        className="text-gray-600 hover:text-blue-500"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaTwitter size={24} />
+      </a>
+
+      {/* Facebook */}
+      <a
+        href="https://www.facebook.com/profile.php?id=61558545486623"
+        aria-label="Facebook"
+        className="text-gray-600 hover:text-blue-500"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaFacebook size={24} />
+      </a>
+
+      {/* Instagram */}
+      <a
+        href="https://www.instagram.com/mharisawan27"
+        aria-label="Instagram"
+        className="text-gray-600 hover:text-blue-500"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaInstagram size={24} />
+      </a>
+
+      {/* LinkedIn */}
+      <a
+        href="https://www.linkedin.com/in/m-haris-awan/"
+        aria-label="LinkedIn"
+        className="text-gray-600 hover:text-blue-500"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin size={24} />
+      </a>
+    </div>
+  );
+};
+
+export default SocialLinks;
